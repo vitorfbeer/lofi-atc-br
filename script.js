5739bbc4a1873a45a19c6ea17557e4e1449f13f5
Vitor Beer <vitorbeer92@gmail.com>
2025-02-22 12:54:28 -0300

    controle de volume separado

--- script.js
+++ script.js
@@ -8,12 +8,24 @@ function onYouTubeIframeAPIReady() {
     atcPlayer = new YT.Player('atcPlayer', { events: { 'onReady': onPlayerReady } });
 }
 
-// Quando o player estiver pronto, define o volume
+// Quando o player estiver pronto, define o volume do YouTube
 function onPlayerReady(event) {
     event.target.setVolume(50);
 }
 
-// Play/Pause sincronizado
+// Função para atualizar o volume do ATC
+const atcVolumeControl = document.getElementById("atcVolume");
+atcVolumeControl.addEventListener("input", (event) => {
+    atcAudio.volume = event.target.value / 100;  // O valor do volume é de 0 a 1
+});
+
+// Função para atualizar o volume do Lo-Fi
+const lofiVolumeControl = document.getElementById("lofiVolume");
+lofiVolumeControl.addEventListener("input", (event) => {
+    atcPlayer.setVolume(event.target.value);  // O volume do YouTube vai de 0 a 100
+});
+
+// Função Play/Pause sincronizada
 playPauseButton.addEventListener("click", () => {
     if (!isPlaying) {
         atcPlayer.playVideo();
